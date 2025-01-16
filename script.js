56e6f428d31e942e5eeb69b2b7864657fda6c786
Danilo Lopes <contatolopesdan@gmail.com>
2025-01-16 09:31:29 -0300

    add animationScroll and ScrollSmooth

--- script.js
+++ script.js
@@ -24,12 +24,10 @@ initTabNav();
 
 function initAccordion() {
   const accordionList = document.querySelectorAll('.js-accordion dt');
-  const activeClass = document.querySelectorAll('.active');
 
   if (accordionList.length) {
     accordionList[0].classList.add('active');
     accordionList[0].nextElementSibling.classList.add('acitve'); // select DD
-
     function accordionClick() {
       this.classList.toggle('active');
       this.nextElementSibling.classList.toggle('active');
@@ -41,3 +39,47 @@ function initAccordion() {
 }
 
 initAccordion();
+
+function initScrollSmooth() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  linksInternos.forEach((link) => {
+    function scrollToSection(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute('href');
+      const section = document.querySelector(href);
+
+      section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+    link.addEventListener('click', scrollToSection);
+  });
+}
+
+initScrollSmooth();
+
+function initAnimationScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+
+  if (sections.length > 0) {
+    const windowMiddle = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const inSectionVisible = sectionTop - windowMiddle < 0;
+        if (inSectionVisible) section.classList.add('active');
+        else {
+          section.classList.remove('active');
+        }
+      });
+    }
+    animaScroll();
+
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+
+initAnimationScroll();
